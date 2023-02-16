3b2a78f8d4282962e8e282b711fe4a480cce109a
Artem <artem@ryzhenko.com>
2023-02-16 19:20:01 +0200

    deploy img

--- lesson-9/deploy.py
+++ lesson-9/deploy.py
@@ -1,12 +1,27 @@
 import boto3
 
-ec2 = boto3.resource('ec2', region_name='us-east-1')
-filters = [
-    {'Name':'tag:proj', 'Values': ['*hillel-lesson*']},
-    {'Name':'tag:env', 'Values': ['*dev*']}
-]
 
-response = ec2.instances.filter(Filters=filters)
+def get_ips():
+    ec2 = boto3.resource('ec2', region_name='us-east-1')
+    filters = [
+        {'Name': 'tag:proj', 'Values': ['*hillel-lesson*']},
+        {'Name': 'tag:env', 'Values': ['*dev*']},
+    ]
 
-for instance in response:
-    print(instance.public_ip_address)
+    response = ec2.instances.filter(Filters=filters)
+
+    for instance in response:
+        yield instance.public_ip_address
+
+
+def deploy(ip):
+    """
+    Go via ssh to machine and deploy it
+    """
+    print(f'Starting deploy to {ip}')
+
+
+def do_deploy():
+    for ip in get_ips():
+        deploy(ip)
+
